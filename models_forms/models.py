232c69162282a2ca6ce7dec4fe5f86fbf6893b6a
brenden <brendenjarias@gmail.com>
2023-11-12 21:32:39 -0800

    messed up the venv so updated that trying build again

--- models_forms/models.py
+++ models_forms/models.py
@@ -2,7 +2,6 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from sqlalchemy.schema import UniqueConstraint
 
